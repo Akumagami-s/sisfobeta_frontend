c372f4984ad19f3ef8b236511cb45899546037c1
alfito <muhammadalfitorahman@gmail.com>
2021-09-16 06:42:25 +0700

    fixed responsive login page for mobile

--- assets/js/chartjs/ui-chart.js
+++ assets/js/chartjs/ui-chart.js
@@ -1,3 +1,4 @@
+/* Chart Prajurit Actif or no */
 const data = {
     labels: [
       'Prajurit Aktif',
@@ -18,10 +19,110 @@ const data = {
     type: 'doughnut',
     data: data,
     options : {
+      legend: {
+        display: false
+      },
     }
   };
 
   var myChart = new Chart(
     document.getElementById('rekapChart'),
     config
+  );
+
+
+
+  /* Chart total terima (Ebaltab) */
+
+const dataTotalTerima = {
+  labels: [
+    'Total Di Terima'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [20,0],
+    backgroundColor: [
+      '#7334FF'
+    ],
+    hoverOffset: 4
+  }]
+};
+
+const configTotalTerima = {
+  type: 'doughnut',
+  data: dataTotalTerima,
+  options : {
+    legend: {
+      display: false
+    },
+  }
+};
+
+var myChart = new Chart(
+  document.getElementById('totalPenerima'),
+  configTotalTerima
+);
+
+  /* Chart sudah Diterima (Ebaltab) */
+
+
+const dataSudahDiterima = {
+  labels: [
+    'Sudah Di Terima',
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [30],
+    backgroundColor: [
+      '#DC3546',
+    ],
+    hoverOffset: 4
+  }]
+};
+
+const configSudahDiterima = {
+  type: 'doughnut',
+  data: dataSudahDiterima,
+  options : {
+    legend: {
+      display: false
+    },
+  }
+};
+
+var myChart = new Chart(
+  document.getElementById('sudahDiterima'),
+  configSudahDiterima
+);
+
+/* Chart Data Alokasi */
+  const dataAlokasi = {
+    labels: [
+      'Jumlah Dana Alokasi',
+      'Dana Alokasi Terealisasi'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [90,20],
+      backgroundColor: [
+        '#7334FF',
+        '#FF832B',
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  const configDataAlokasi = {
+    type: 'doughnut',
+    data: dataAlokasi,
+    options : {
+      legend: {
+        display: false
+      },
+    }
+  };
+
+  var myChart = new Chart(
+    document.getElementById('chartDataAlokasi'),
+    configDataAlokasi
   );
--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -3,3 +3,22 @@ document.querySelector(".searchIcon").addEventListener("click",function(){
     document.querySelector(".searchInput").classList.toggle("active");
 });
 
+
+$(document).ready(function(){
+    $('.wrappercard-statistic').slick({
+        arrows : false,
+        slidesToShow : 2,
+        infinite : false,
+        responsive: [
+            {
+              breakpoint: 576,
+              settings: {
+                slidesToShow: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
+});

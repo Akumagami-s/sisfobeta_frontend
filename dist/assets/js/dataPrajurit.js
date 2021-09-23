4021038c46a12d6acb87780ce46e4286922f21cd
alfito <muhammadalfitorahman@gmail.com>
2021-09-23 12:01:19 +0700

    data prajurit done

--- dist/assets/js/dataPrajurit.js
+++ dist/assets/js/dataPrajurit.js
@@ -1,3 +1,9 @@
 $(document).ready( function () {
-    $('#tablePokokPrajurit').DataTable();
+    $('#tablePokokPrajurit').DataTable({
+        scrollX : true,
+    });
 } );
+
+document.querySelector(".searchIcon").addEventListener("click",function(){
+    document.querySelector(".searchInput").classList.toggle("active");
+});

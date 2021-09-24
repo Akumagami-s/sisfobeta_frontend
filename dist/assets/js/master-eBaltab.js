ca938bdb74c5c322f7ead39845c5e21c260965e5
alfito <muhammadalfitorahman@gmail.com>
2021-09-24 20:41:54 +0700

    ebaltab Done

---
+++ dist/assets/js/master-eBaltab.js
@@ -0,0 +1,50 @@
+$(document).ready( function () {
+    $('#tableMasterInstitutPangkat').DataTable({
+        scrollX : true,
+    });
+} );
+$(document).ready( function () {
+    $('#tableMasterInstitutKesatuan').DataTable({
+        scrollX : true,
+    });
+} );
+
+
+$(document).ready( function () {
+    $('#tableMasterInstitutKorp').DataTable({
+        scrollX : true,
+    });
+} );
+
+
+
+$(document).ready( function () {
+    $('#tableMasterKategori').DataTable({
+        scrollX : true,
+    });
+} );
+
+$(document).ready( function () {
+    $('#tableMasterKotama').DataTable({
+        scrollX : true,
+    });
+} );
+
+$(document).ready( function () {
+    $('#tableMasterSatminkal').DataTable({
+        scrollX : true,
+    });
+} );
+
+$(document).ready( function () {
+    $('#tableMasterBunga').DataTable({
+        scrollX : true,
+    });
+} );
+
+$(document).ready( function () {
+    $('#tableMasterPotongan').DataTable({
+        scrollX : true,
+    });
+} );
+

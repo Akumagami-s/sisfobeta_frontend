5898b3c2a15509bedd313078f37ffc20fe0511fe
alfito <muhammadalfitorahman@gmail.com>
2021-09-29 15:53:23 +0700

    30% dashboard EKPR

---
+++ dist/assets/js/dashboard-ekpr.js
@@ -0,0 +1,14 @@
+// $('.wrapperDebitur').slick({
+//     infinite: false,
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
+//     arrows : false,
+//     responsive: [
+//       {
+//         breakpoint: 576,
+//         settings: {
+//           slidesToShow: 1
+//         }
+//       }
+//     ]
+//   });

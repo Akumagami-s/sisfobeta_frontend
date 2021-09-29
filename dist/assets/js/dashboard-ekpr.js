583ceaaa746d06163d3280eaeb10fb673051f4ac
alfito <muhammadalfitorahman@gmail.com>
2021-09-30 00:13:14 +0700

    dashboard Ekpr Progres 50%

--- dist/assets/js/dashboard-ekpr.js
+++ dist/assets/js/dashboard-ekpr.js
@@ -1,14 +1,22 @@
-// $('.wrapperDebitur').slick({
-//     infinite: false,
-//     slidesToShow: 2,
-//     slidesToScroll: 1,
-//     arrows : false,
-//     responsive: [
-//       {
-//         breakpoint: 576,
-//         settings: {
-//           slidesToShow: 1
-//         }
-//       }
-//     ]
-//   });
+$('.wrapperDataMassDebet').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows : false,
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
+  });

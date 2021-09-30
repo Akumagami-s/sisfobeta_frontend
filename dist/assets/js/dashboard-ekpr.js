a2740a0da1c6bf1dd212c84ffeadc738c64b62fa
alfito <muhammadalfitorahman@gmail.com>
2021-10-01 00:22:16 +0700

    dashboard e-KPR Done

--- dist/assets/js/dashboard-ekpr.js
+++ dist/assets/js/dashboard-ekpr.js
@@ -7,16 +7,57 @@ $('.wrapperDataMassDebet').slick({
         {
           breakpoint: 768,
           settings: {
-            slidesToShow: 2,
+            slidesToShow: 1,
             slidesToScroll: 1,
+            centerMode: true,
+            centerPadding: '40px',
           }
         },
         {
-          breakpoint: 576,
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: true,
+            centerPadding: '30px',
+          }
+        },
+      ]
+  });
+
+
+
+
+
+  $('.wrapperStatistic').slick({
+    infinite: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows : false,
+    responsive: [
+        {
+          breakpoint: 768,
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
           }
-        }
+        },
       ]
   });
+
+  $(document).ready(function() {
+    $('#detailDataBRI').DataTable( {
+      "scrollX": true,
+      "paging": false,
+
+    });
+} );
+
+
+
+$(document).ready(function() {
+  $('#detailDataBTN').DataTable( {
+    "scrollX": true,
+    "paging":   false,
+  });
+} );

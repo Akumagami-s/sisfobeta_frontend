62073acd0242c1cd3492886b49708f70ccd97d9f
alfito <muhammadalfitorahman@gmail.com>
2021-10-04 06:47:10 +0700

    benerin tugas yoga

--- dist/assets/js/kelolaUser.js
+++ dist/assets/js/kelolaUser.js
@@ -64,3 +64,34 @@ $('button[data-bs-toggle="tab"]').on('shown.bs.tab', function(e){
     $($.fn.dataTable.tables(true)).DataTable()
         .columns.adjust();
   });
+
+//for slick BACA YOGA
+  $('.menu').slick({
+    infinite: false,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    arrows : false,
+    responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+          }
+        },
+      ]
+  });
+
+
+//for sticky menu BACA YOGA
+  $(document).scroll(function(){
+    $('.pembungkus').toggleClass('onScrolled', $(this).scrollTop() > 170);
+  });
+

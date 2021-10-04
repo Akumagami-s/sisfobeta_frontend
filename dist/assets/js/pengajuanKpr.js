41960e0933a11c3c8fe566d8a6cd4acdbca6e249
Yogapratama1109 <yoga110011@gmail.com>
2021-10-05 01:10:41 +0700

    update:menu KPR

--- dist/assets/js/pengajuanKpr.js
+++ dist/assets/js/pengajuanKpr.js
@@ -16,6 +16,35 @@ $(document).ready( function () {
 } );
 
 
+$('.menu').slick({
+    infinite: false,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    arrows : false,
+    responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+          }
+        },
+      ]
+  });
+
+
+  $(document).scroll(function(){
+    $('.pembungkus').toggleClass('onScrolled', $(this).scrollTop() > 170);
+  });
+
+
 $('button[data-bs-toggle="tab"]').on('shown.bs.tab', function(e){
     $($.fn.dataTable.tables(true)).DataTable()
         .columns.adjust();
